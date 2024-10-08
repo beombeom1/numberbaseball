f31a08f9919d5ef0427a7ac5eda1b296e6325999
beombeom1 <jebeam3632@naver.com>
2024-10-08 09:25:24 +0900

    숫자야구 디테일추가

--- src/test2.java
+++ src/test2.java
@@ -24,13 +24,21 @@ public class test2 {
         numbers.add(c);
         System.out.println("숫자 야구 3자리");
         int randomNumber = 100 * a + 10 * b + c;
-        System.out.println(randomNumber);
+        //System.out.println(randomNumber);
         while (true){
             Scanner sc = new Scanner(System.in);
             int s = sc.nextInt();
             x = s / 100;
             y = (s / 10) % 10;
             z = s % 10;
+            if(s<100){
+                System.out.println("3자리 숫자를 입력해주세요");
+                continue;
+            }
+            if(x==y||y==z||x==z){
+                System.out.println("서로 다른 숫자를 입력해주세요");
+                continue;
+            }
             ArrayList<Integer> numbers2 = new ArrayList<>();
             numbers2.add(x);
             numbers2.add(y);
